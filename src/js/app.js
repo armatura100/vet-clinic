1cf27f255494d14f8bb040773bee30f16199a8d1
Armatura <oleksandrtiumentsev@gmail.com>
2022-10-25 16:15:23 +0300

    make header and footer adaptive version

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,28 @@
 import * as flsFunctions from './modules/functions.js';
 flsFunctions.isWebp()
+
+const phonesMobileBtn = document.querySelector('.phones-mobile__btn');
+const phonesMobileList = document.querySelector('.phones-mobile__list');
+
+phonesMobileBtn.addEventListener('click', () => {
+    phonesMobileList.classList.toggle('active');
+});
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+const menuLinks = document.querySelectorAll('.menu__link');
+const body = document.body;
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    menuList.classList.toggle('active');
+    body.classList.toggle('lock');
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuBtn.classList.remove('active');
+        menuList.classList.remove('active');
+        body.classList.remove('lock');
+    });
+});
