3bd2733f288b1f660db2afd6afc6fa4dcd10c7f2
Armatura <oleksandrtiumentsev@gmail.com>
2022-10-27 00:00:23 +0300

    freaking bug again

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-import * as flsFunctions from './modules/functions.js';
-flsFunctions.isWebp()
+import * as webpFunction from './modules/webpFunction.js';
+webpFunction.isWebp();
 
 const phonesMobileBtn = document.querySelector('.phones-mobile__btn');
 const phonesMobileList = document.querySelector('.phones-mobile__list');
@@ -26,3 +26,34 @@ menuLinks.forEach(link => {
         body.classList.remove('lock');
     });
 });
+
+import Swiper, { Navigation, Thumbs } from 'swiper';
+
+if (document.querySelector('.services-slider__cards')) {
+    const dots = new Swiper('.services-slider__cards', {
+        modules: [Navigation],
+        navigation: {
+            nextEl: '.services-slider .swiper-button-next',
+            prevEl: '.services-slider .swiper-button-prev',
+        },
+        slidesPerView: 6,
+        spaceBetween: 13,
+        loop: true,
+        watchSlidesProgress: true,
+        simulateTouch: false
+    });
+    new Swiper('.services-slider__display', {
+        modules: [Thumbs],
+        thumbs: {
+            swiper: dots,
+        },
+        loop: true
+    });
+}
+
+if (document.querySelector('.related-slider__body')) {
+    new Swiper('.related-slider__body', {
+        slidesPerView: 2,
+        spaceBetween: 24
+    });
+}
